44d08f8c0dbb9452c16b6c38577e53cf249fa1ed
unknown <h2magic@163.com>
2020-08-27 16:47:10 +0800

    Init Technology class

--- Common/Technology.h
+++ Common/Technology.h
@@ -1,9 +1,19 @@
 #ifndef TECHNOLOGY_H
 #define TECHNOLOGY_H
+#include <QString>
 
+enum { MILITARY, CIVIAL, BEYOND };
 
 class Technology
 {
+    QString name;
+    QString description;
+    int cost;
+    int area; // 军事 0、民用 1、超越 2
+
+    bool isLoop = false;
+    int loop = 1;
+
 public:
     Technology();
 };
